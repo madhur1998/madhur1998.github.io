0653ee453a648c4ab9b16d030eb6a2b816dc3878
madhur1998 <madhurgupta26@gmail.com>
2017-03-26 06:09:39 +0530

    Update javascript.js

--- Web App/js/javascript.js
+++ Web App/js/javascript.js
@@ -21,15 +21,17 @@ $(document).ready(function() {
 			});
 
 		}else{
-			$("error").html('this field cannot be empty');
+			window.alert("Name of city cannot be emty.");
 		}
 	});
 });
 
 function show(response){
 
-	return "<h2 style='font-size:30px; font-weight:bold;' class='text-output'><strong>City</strong>: "+ response.name +"</h2><br>"+
-	"<h3><strong>Temprature</strong>: "+ response.main.temp +"</h3><br>"+
-	"<h3><strong>Humidity</strong>: "+ response.main.humidity +"</h3><br>"+
-	"<h3><strong>Description</strong>: "+ response.weather[0].decription +"</h3><br>";
+	return "<h2 style='font-size:30px; font-weight:bold;' class='text-output'><strong>City</strong>: <a target='_blank' class='link-text' href='https://en.wikipedia.org/wiki/" + response.name + "'>" + response.name +"</a></h2><br>"+
+	"<h3><strong>Temprature</strong>: <img src='http://openweathermap.org/img/w/" + response.weather[0].icon + ".png'>&nbsp" + response.main.temp +"&deg;C</h3><br>"+
+	"<h3><strong>Description</strong>: "+ response.weather[0].description + "</h3><br>" +
+	"<h3><strong>Humidity</strong>: "+ response.main.humidity +"%</h3><br>"+
+	"<h3><strong>Wind Speed</strong>: "+ response.wind.speed +"m/s</h3><br>";
 }
+
